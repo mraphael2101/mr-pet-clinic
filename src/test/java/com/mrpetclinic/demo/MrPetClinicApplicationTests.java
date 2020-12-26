53db3d6b5e3391625cdf2ea98a020f8165ce50aa
mraphael2101 <mark.raphael@ymail.com>
2020-12-26 16:06:28 +0000

    Initial Project Add

---
+++ src/test/java/com/mrpetclinic/demo/MrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrpetclinic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
