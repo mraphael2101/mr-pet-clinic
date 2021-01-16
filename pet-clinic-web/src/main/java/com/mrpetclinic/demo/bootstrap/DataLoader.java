31efdd8958ef2b1c6f9b6ba48247815301ae5e30
mraphael2101 <mark.raphael@ymail.com>
2021-01-16 08:29:57 +0000

    Added PetTypes to Bootstrap

--- pet-clinic-web/src/main/java/com/mrpetclinic/demo/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/mrpetclinic/demo/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.mrpetclinic.demo.bootstrap;
 
 import com.mrpetclinic.demo.model.Owner;
+import com.mrpetclinic.demo.model.PetType;
 import com.mrpetclinic.demo.model.Vet;
 import com.mrpetclinic.demo.services.OwnerService;
+import com.mrpetclinic.demo.services.PetTypeService;
 import com.mrpetclinic.demo.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,15 +15,25 @@ public class DataLoader implements CommandLineRunner {
     
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     // Constructor based dependency injection to Spring App Context - Inversion of Control
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setId(1L);
