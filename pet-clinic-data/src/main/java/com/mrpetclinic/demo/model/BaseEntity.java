d9522b0c3732cbd6ab18b20f971a31cd438e332e
mraphael2101 <mark.raphael@ymail.com>
2021-01-31 17:38:25 +0000

    Update base entity with JPA annotations.

--- pet-clinic-data/src/main/java/com/mrpetclinic/demo/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mrpetclinic/demo/model/BaseEntity.java
@@ -1,15 +1,23 @@
 package com.mrpetclinic.demo.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-/* Reminder: Serialization is a mechanism for converting an object into a byte stream.
+/* BaseEntity is a JPA concept that has specific annotations.
+   Reminder: Serialization is a mechanism for converting an object into a byte stream.
    Deserialization is the reverse process where the byte stream is used to recreate the
    actual object in memory.
+*/
 
-   BaseEntity is a JPA concept that has specific annotations */
-
+// Establishes this class as a Base Class for JPA
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -19,4 +27,5 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
 }
