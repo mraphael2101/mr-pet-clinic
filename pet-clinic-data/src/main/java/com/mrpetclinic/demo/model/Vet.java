e954c21b952265a33de0dfdeb4e9165db95b9295
mraphael2101 <mark.raphael@ymail.com>
2021-01-31 22:35:59 +0000

    Updated vet to use JPA

--- pet-clinic-data/src/main/java/com/mrpetclinic/demo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mrpetclinic/demo/model/Vet.java
@@ -1,10 +1,17 @@
 package com.mrpetclinic.demo.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    // FetchType.EAGER means that JPA will try to load everything all at once
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
